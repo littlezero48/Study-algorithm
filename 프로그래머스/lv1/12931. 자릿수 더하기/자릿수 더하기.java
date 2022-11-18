3691e36e41f8c63041d6a7762a9d059a3f334f20
littlezero48 <111271565+littlezero48@users.noreply.github.com>
2022-11-18 22:39:52 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.13 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -1,11 +1,13 @@
-import java.util.*;
-
 public class Solution {
     public int solution(int n) {
         int answer = 0;
 
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
+        String a = Integer.toString(n);
+        String[] s_arr = a.split("");
+
+        for(int i=0; i<s_arr.length; i++){
+            answer += Integer.parseInt(s_arr[i]);
+        }
 
         return answer;
     }
