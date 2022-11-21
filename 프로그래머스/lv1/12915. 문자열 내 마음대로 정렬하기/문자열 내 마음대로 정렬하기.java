b105751747d24156224457ca87b3be1684166072
littlezero48 <111271565+littlezero48@users.noreply.github.com>
2022-11-21 22:49:30 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 2.01 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -4,12 +4,21 @@ class Solution {
         String[] answer = new String[strings.length];
 
         for(int i=0; i<strings.length; i++){
-            answer[i] = strings[i].substring(n,n+1) + strings[i];
+            if(n == 0) {
+                answer[i] = strings[i] + i;
+            } else if (n == strings[i].length()){
+                answer[i] = strings[i].substring(n,n+1) + strings[i].substring(0,n) + i;
+            } else {
+                answer[i] = strings[i].substring(n,n+1) + strings[i].substring(0,n) + strings[i].substring(n+1)  + i;
+            }
         }
         Arrays.sort(answer);
 
         for(int j=0; j<answer.length; j++){
-            answer[j] = answer[j].substring(1);
+
+            int answerLen = answer[j].length();
+            int index = Integer.parseInt(answer[j].substring(answerLen-1));
+            answer[j] = strings[index];
         }
 
         return answer;
