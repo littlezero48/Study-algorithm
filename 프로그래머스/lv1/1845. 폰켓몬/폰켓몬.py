0e65aeec08303f50698c86533a3e8ba1646eaf0c
littlezero48 <111271565+littlezero48@users.noreply.github.com>
2023-01-16 18:44:21 +0900

    [level 1] Title: 폰켓몬, Time: 0.19 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,9 +1,5 @@
 def solution(nums):
-    answer = 0
     all_nums = len(set(nums))
-    choice = len(nums) //2
-    if(all_nums > choice):
-        answer = choice
-    else :
-        answer = all_nums
+    choice = len(nums) // 2
+    answer = choice if all_nums > choice else all_nums 
     return answer
