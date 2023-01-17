1a8abeed21b090a3a10156a87639ac81676126dc
littlezero48 <111271565+littlezero48@users.noreply.github.com>
2023-01-18 01:48:37 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -3,17 +3,19 @@ def solution(progresses, speeds):
     day = 0
     answer = []
     
-    while len(progresses) > 0 :
-        if (progresses[0] + speeds[0] * day) >= 100 :
-            release_cnt = release_cnt + 1
-            progresses.pop(0)
-            speeds.pop(0)
-        else :
-            if release_cnt > 0 :
-                answer.append(release_cnt)
-                release_cnt = 0
-            day = day + 1
-    answer.append(release_cnt) 
+    while len(progresses) > 0 :                         # progresses배열이 존재하는 한 계속 진행
+        if (progresses[0] + speeds[0] * day) >= 100 :   # 100을 달성하면
+            release_cnt = release_cnt + 1               # cnt 추가
+            progresses.pop(0)                           # 배열에서 제거하므로써 while을 줄여감
+            speeds.pop(0)                               # 배열에서 제거
+        else :                                          # 100 달성 못하면
+            if release_cnt > 0 :                        # cnt가 0이면 그냥 패스
+                                                        # cnt가 존재하고 100달성을 못한 걸 만났다면 이전 달성케이스까지를 answer에 append
+                answer.append(release_cnt)      
+                release_cnt = 0                         # 다시 cnt 초기화
+            day = day + 1                               # 조건 달성을 위해 day를 하나하나 늘려감 
+    
+    answer.append(release_cnt)                          # 배열 모두 pop하고 처리되지 못한 마지막 결과를 넣기 위한 append              
             
     return answer
 
