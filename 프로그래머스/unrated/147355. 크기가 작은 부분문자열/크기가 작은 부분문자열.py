51a327008ce19e4c7503a902c1251a64d7e672c8
littlezero48 <111271565+littlezero48@users.noreply.github.com>
2023-01-31 17:20:42 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
@@ -0,0 +1,16 @@
+def solution(t, p):
+    
+    cnt = 0
+    # len 길이 구하는 식
+    unit = len(p)
+    # range(min, max, gap) gap은 숫자들 사이에 차이를 두고 만든다.
+    # 한줄 포문 앞에 실행문 두고 뒤에 for문을 둔다.
+    # 리스트 표현식에는 리스트 안에 if문과 for을 넣을 수 있음
+    b = [t[i:i+unit] for i in range(0,len(t))]
+    print(b)
+
+    for one in b:
+        if int(one) <= int(p) and len(one) == len(p):
+            cnt += 1
+
+    return cnt
