844432561e7bb90dcdbcb2027f32a331034dde0d
littlezero48 <111271565+littlezero48@users.noreply.github.com>
2023-02-22 01:33:14 +0900

    [unrated] Title: 둘만의 암호, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
@@ -1,11 +1,3 @@
 def solution(s, skip, index):
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-    result = ""
-
-    for i in skip :
-        alphabet = alphabet.replace(i,'')
-
-    for one in s :
-        result += alphabet[(alphabet.find(one) + index) % len(alphabet)]
-
-    return result
+    answer = ''
+    return answer
