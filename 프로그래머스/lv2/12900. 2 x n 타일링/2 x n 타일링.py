b40344b9ddb522a344b5314cc28764e38ef64ea8
littlezero48 <111271565+littlezero48@users.noreply.github.com>
2023-08-06 15:59:06 +0900

    [level 2] Title: 2 x n 타일링, Time: 13.92 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.py
+++ 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.py
@@ -1,12 +1,12 @@
 def solution(n):
     
     # 동적계획법 DP 사용방식
-    DP = [0 for i in range(n)]      #이전 경우 수 기록용 배열 
+    DP = [0 for i in range(n)]                      #이전 경우 수 기록용 배열 
     
     DP[0] = 1
     DP[1] = 2
     
-    for i in range(2,n):            #방식에 들어가지 못하는 0,1 인덱스 제외 그 이후부터 
-        DP[i] = (DP[i-1] + DP[i-2]) % 1000000007  #10억7을 나누는 이유가 뭘까 
+    for i in range(2,n):                            #방식에 들어가지 못하는 0,1 인덱스 제외 그 이후부터 
+        DP[i] = (DP[i-1] + DP[i-2]) % 1000000007    #10억7을 나누는 이유가 뭘까 
         
-    return DP[n-1]                  # 마지막 경우의 수 
+    return DP[n-1]                                  # 마지막 경우의 수 
